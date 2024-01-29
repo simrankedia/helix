94edf3beeef334cfc6a1ed4da240b685533257f3
Pascal Kuthe <pascalkuthe@pm.me>
2024-01-29 17:11:00 +0100

    render diagnostic inline

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -1681,6 +1681,7 @@ pub fn scroll(cx: &mut Context, offset: usize, direction: Direction, sync_cursor
                 &mut annotations,
             )
         });
+        drop(annotations);
         doc.set_selection(view.id, selection);
         return;
     }
